485b3506d283fd6439a18a2f9a70879764ab06b2
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2024-09-18 17:59:07 +0200

    Added UI scenes for main menu and game over screen

---
+++ UI/MainMenu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_pressed():
+	TransitionManager.change_scene("res://Main.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
