2aa4cfd92f65da53096ef08d6415eb162876788f
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2024-09-19 14:42:47 +0200

    Merge

--- Character.gd
+++ Character.gd
@@ -3,8 +3,23 @@ class_name Character
 
 @export var Sprite : AnimatedSprite2D
 
+var clothing_type : WeatherObj.WeatherType = WeatherObj.WeatherType.SUN
+
 func _ready() -> void:
 	Sprite.animation = "Snow"
 	Sprite.play()
-	
-	
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("switch"):
+		clothing_type += 1
+		if clothing_type > 2:
+			clothing_type = 0
+		
+		match clothing_type:
+			WeatherObj.WeatherType.SUN: 
+				Sprite.animation = "Sun"
+			WeatherObj.WeatherType.SNOW: 
+				Sprite.animation = "Snow"
+			WeatherObj.WeatherType.RAIN: 
+				Sprite.animation = "Rain"
+		Sprite.play()
--- Scripts/backgroundmoving.gd
+++ Scripts/backgroundmoving.gd
@@ -1,33 +1,35 @@
 extends Node2D
 
+@export var Background1 : Sprite2D
+@export var Background2 : Sprite2D
+
 # Speed of the background movement
 var speed: float = 200.0
 
 # Backgrounds inititialization
-var background1
-var background2
 var initial_position1
 var initial_position2
 
 func _ready():
 	# Assign the background sprites 
-	background1 = $Background1
-	background2 = $Background2
-	initial_position1 = background1.position.x
-	initial_position2 = background2.position.x
+	initial_position1 = Background1.position.x
+	initial_position2 = Background2.position.x
 	
-
+	var sprite_end_pos_1 := Background1.position.x + Background1.texture.get_width() / 2
+	var sprite_end_pos_2 := Background2.position.x + Background2.texture.get_width() / 2
+	
 func _process(delta: float):
 	# Move the backgrounds to the left
-	background1.position.x -= speed * delta
-	background2.position.x -= speed * delta
-
-
+	Background1.position.x -= speed * delta
+	Background2.position.x -= speed * delta
+	
+	
+	print(Background1.position.x + Background1.texture.get_width() * 10)
 	# Reset position when one background goes off-screen
-	if background1.position.x < -get_viewport_rect().size.x / 2:
-		background1.position.x = initial_position2
-	if background2.position.x < -get_viewport_rect().size.x / 2:
-		background2.position.x = initial_position2
+	if 0 > Background1.position.x + Background1.texture.get_width() * 10:
+		Background1.position.x = Background2.position.x + Background2.texture.get_width()
+	if 0 > Background2.position.x + Background2.texture.get_width() * 10:
+		Background2.position.x = Background1.position.x + Background1.texture.get_width()
 		
 
 	
